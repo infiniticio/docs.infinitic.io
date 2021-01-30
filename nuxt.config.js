b6647934b6b1145d7a8f95ba86a138e809f3a6d9
Gilles Barbier <gilles@zenaton.com>
2021-01-30 16:18:24 +0100

    add favicons and logos

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,5 +11,31 @@ export default theme({
         theme: 'prism-themes/themes/prism-darcula.css'
       }
     }
+  },
+  head: {
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicons/favicon-16x16.png',
+        sizes: '16x16'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicons/favicon-32x32.png',
+        sizes: '32x32'
+      },
+      {
+        rel: 'apple-touch-icon',
+        type: 'image/png',
+        href: '/favicons/apple-touch-icon.png',
+        sizes: '180x180'
+      },
+      {
+        rel: 'manifest',
+        href: '/favicons/site.webmanifest'
+      }
+    ],
   }
 })
