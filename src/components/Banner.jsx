1fd6d4eeae1abb6237716bfb53a99a150f3eb4ac
Gilles Barbier <gilles@infinitic.io>
2023-12-13 10:24:56 +0100

    new banner 0.12.1

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,38 +1,25 @@
 import { MegaphoneIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
-const URL = "https://infinitic.substack.com/p/state-of-infinitic-v0112"
-const TITLE_SMALL = "New version 0.11.2!"
-const TITLE = "Version 0.11.2 released! What's new for Infinitic and what's next."
+const URL = "https://open.substack.com/pub/infinitic/p/new-release-v0121"
+const TITLE_SMALL = "New version 0.12.1!"
+const TITLE = "What's new for Infinitic and what's next"
 
 export  function Banner() {
   return (
-    <>
-      <div className="inset-x-0 bottom-0">
-        <div className="bg-slate-900 dark:bg-slate-800">
-          <div className="mx-auto max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
-            <div className="flex flex-wrap items-center justify-between">
-              <div className="flex w-0 flex-1 items-center">
-                <span className="flex rounded-lg bg-blue-800 p-2">
-                  <MegaphoneIcon className="h-6 w-6 text-white" aria-hidden="true" />
-                </span>
-                <p className="ml-3 truncate font-medium text-white">
-                  <span className="md:hidden">{ TITLE_SMALL }</span>
-                  <span className="hidden md:inline">{ TITLE }</span>
-                </p>
-              </div>
-              <div className="order-3 mt-2 w-full flex-shrink-0 sm:order-2 sm:mt-0 sm:w-auto">
-                <a
-                  href={ URL }
-                  className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-2 text-sm font-medium text-blue-600 shadow-sm hover:bg-indigo-50"
-                >
-                  <span className='sm:hidden'>{TITLE_SMALL}</span>
-                  <span className='hidden sm:inline'>Learn More</span>
-                </a>
-              </div>
-            </div> 
-          </div>
-        </div>
-      </div>
-    </>
+<div class="flex items-center gap-x-6 bg-pink-200 dark:bg-pink-800 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+  <p class="text-sm leading-6 text-black dark:text-white">
+    <a href={ URL }>
+      <strong class="font-semibold">{ TITLE_SMALL }</strong>
+      <span className='sm:hidden'>
+      <svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"></svg><span aria-hidden="true">&rarr;</span>
+      </span>
+      <span className='hidden sm:inline'>
+        <svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg>{ TITLE }&nbsp;<span aria-hidden="true">&rarr;</span>
+      </span>
+    </a>
+  </p>
+  <div class="flex flex-1 justify-end"></div>
+</div>
+
   )
 }
